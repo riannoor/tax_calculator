b7f71f32573add62efe3c148fac9f5967eb6dfa2
Rian Noor <noor.m.fakhrian@gmail.com>
2019-01-22 09:21:10 +0700

    (+) initial commit

---
+++ migration.js
@@ -0,0 +1,89 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL
+});
+
+/**
+ * run migrations and seeds
+ */
+
+const up = () => {
+  /** created tables */
+  const queryText =`
+    CREATE TABLE IF NOT EXISTS
+    master_tax_codes(
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(255) NOT NULL,
+      created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+      modified_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+    ); 
+    CREATE TABLE IF NOT EXISTS
+    bills(
+      id SERIAL PRIMARY KEY,
+      tax_code_id SERIAL REFERENCES master_tax_codes(id) ON DELETE CASCADE ON UPDATE CASCADE,
+      name VARCHAR(255) NOT NULL,
+      price INTEGER NOT NULL,
+      created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+      modified_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+      )
+  `;
+
+  pool.query(queryText).then((res) => {
+    console.log(res);
+    //insert seeds when tables is created
+    insertSeeds();
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+}
+
+/**
+ * Seeds table
+ */
+const insertSeeds = () => {
+  const queryText = `
+    INSERT INTO master_tax_codes (name)
+    VALUES
+    ('Food & Beverage'),
+    ('Tobacco'),
+    ('Entertainment')`;   
+
+  pool.query(queryText).then((res) => {
+    console.log(res);
+    pool.end();
+  })
+  .catch((err) => {
+    console.log(err);
+    pool.end();
+  });
+}
+
+/**
+ * Drop Tables
+ */
+const down = () => {
+  const queryText = 'DROP TABLE IF EXISTS master_tax_codes, bills';
+  pool.query(queryText)
+    .then((res) => {
+      console.log(res);
+      pool.end();
+    })
+    .catch((err) => {
+      console.log(err);
+      pool.end();
+    });
+}
+
+pool.on('remove', () => {
+  console.log('client removed');
+  process.exit(0);
+});
+
+module.exports = {
+  down,
+  up
+};
+
+require('make-runnable');
